fd846011888e5695d39137fad619270e65ce5cec
Arya-Pragma <mehtaarya18@gmail.com>
2024-01-25 20:25:29 +0530

    single file download file error solved

--- download_single_file.php
+++ download_single_file.php
@@ -6,15 +6,18 @@ $service_id = $_REQUEST['service_id'];
 $stage_id = $_REQUEST['stage_id'];
 $doc_file = $_REQUEST['doc_file'];
 $file_id = $_REQUEST['file_id'];
+$doc_type = $_REQUEST['doc_type'];
 
+$full_path = fill_file($inq_id, $service_id, $stage_id, $file_id, $doc_file, $doc_type);
 
-$full_path = fill_file($inq_id, $service_id, $stage_id, $file_id, $doc_file);
-// get_variables($doc_file);
-
-
+// Set headers for download
+// header('Content-Type: application/octet');
 header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename=' . $full_path);
+header('Content-Disposition: attachment; filename=' . basename($full_path));
 header('Content-Length: ' . filesize($full_path));
+ob_clean();
+flush();
 readfile($full_path);
-unlink($full_path);
+unlink($full_path);  
+exit;
 ?>
